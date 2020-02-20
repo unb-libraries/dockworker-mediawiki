9f3ea3b56492955fbc32161b774444449bfd27c6
Jacob Sanford <jsanford@unb.ca>
2020-02-20 12:09:49 -0400

    Initial commit

---
+++ src/Robo/Plugin/Commands/MediawikiDeploymentCommands.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Dockworker\Robo\Plugin\Commands;
+
+use Dockworker\Robo\Plugin\Commands\DockworkerLocalCommands;
+
+/**
+ * Defines the commands used to interact with a local Mediawiki application.
+ */
+class MediawikiDeploymentCommands extends DockworkerLocalCommands {
+
+  /**
+   * Provides log checker with ignored log exception items for local Mediawiki.
+   *
+   * @hook on-event dockworker-deployment-log-error-exceptions
+   */
+  public function getErrorLogExceptions() {
+    return [];
+  }
+
+}
